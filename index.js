88286999f220c07c774c599ccee396748d83b1ce
omar hussain <omar.hussain4991@gmail.com>
2019-09-24 22:17:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ function destructivelyRemoveFirstKitten () {
   return kittens.shift ()
 }
 function appendKitten () {
-  return kittens = [...kittens, 'Broom']
+  kittens = [...kittens, 'Broom']
 }
